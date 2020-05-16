605ed183c991316f0a3ecc926c9ab262480af0c9
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-05-16 14:06:11 +0300

    fix Users model password is not reqiured for users with google account

--- server/src/database/models/Users.js
+++ server/src/database/models/Users.js
@@ -9,10 +9,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
+  password: String,
   googleId: String,
 });
 
