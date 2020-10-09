82d75d5cdb2e439f435c3caa474cdc187bb078d7
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-10-09 14:52:22 +0300

    refactored home page - fixed App styles - renamed Home.css to style.css, more convinient this way - added a welcomeing message to explain what the app does - fixed the general style and layout

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
 
+import './style.css';
+
 const Landing = () => {
   const history = useHistory();
 
   return (
-    <>
-      <h1>Welcome</h1>
-      <Button onClick={() => history.push('/login')}>Login</Button>
-      <Button onClick={() => history.push('/signup')}>Sign Up</Button>
-    </>
+    <div className="home_wrapper">
+      <h1 className="home__title">
+        Hi there, login and join a room to start chatting with people around the
+        globe.
+      </h1>
+      <div className="home__btns-wrapper">
+        <Button
+          size="large"
+          shape="round"
+          onClick={() => history.push('/login')}
+        >
+          Login
+        </Button>
+        <Button
+          size="large"
+          shape="round"
+          onClick={() => history.push('/signup')}
+        >
+          Sign Up
+        </Button>
+      </div>
+    </div>
   );
 };
 
